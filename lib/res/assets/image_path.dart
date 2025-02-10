3440ab6b9e90fd2e57d514f31bab5a8729578a85
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-02-10 10:55:23 +0600

    add all image path

---
+++ lib/res/assets/image_path.dart
@@ -0,0 +1,7 @@
+class ImagePath {
+  static String logo = "assets/png/logo.png";
+  static String contract = "assets/png/contract.png";
+  static String netflix = "assets/png/netflix.png";
+  static String download = "assets/png/download.png";
+  static String devices = "assets/png/devices.png";
+}
