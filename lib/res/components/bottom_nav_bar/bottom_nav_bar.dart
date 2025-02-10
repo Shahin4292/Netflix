7dd04abced14db0ccc4468135de9ac3642f66d80
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-02-10 11:30:02 +0600

    add bottomNavBar done

--- lib/res/components/bottom_nav_bar/bottom_nav_bar.dart
+++ lib/res/components/bottom_nav_bar/bottom_nav_bar.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:netflix/view/home_screen/home_screen.dart';
+import 'package:netflix/view/new_&_hot/new_and_hot.dart';
+import 'package:netflix/view/search_screen/search_screen.dart';
 
 class BottomNavBar extends StatelessWidget {
   const BottomNavBar({super.key});
@@ -6,31 +9,33 @@ class BottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          bottomNavigationBar: Container(
-            color: Colors.black,
-            height: 70,
-            child: TabBar(
-              tabs: [
-                Tab(
-                  icon: Icon(Icons.home),
-                  text: 'Home',
-                ),
-                Tab(
-                  icon: Icon(Icons.search),
-                  text: 'Search',
-                ),
-                Tab(
-                  icon: Icon(Icons.photo_library_outlined),
-                  text: 'New & Hot',
-                ),
-              ],
-              indicatorColor: Colors.transparent,
-              labelColor: Colors.white,
-              unselectedLabelColor: Color(0xff999999),
-            ),
+      length: 3,
+      child: Scaffold(
+        bottomNavigationBar: Container(
+          color: Colors.black,
+          height: 70,
+          child: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.home),
+                text: 'Home',
+              ),
+              Tab(
+                icon: Icon(Icons.search),
+                text: 'Search',
+              ),
+              Tab(
+                icon: Icon(Icons.photo_library_outlined),
+                text: 'New & Hot',
+              ),
+            ],
+            indicatorColor: Colors.transparent,
+            labelColor: Colors.white,
+            unselectedLabelColor: Color(0xff999999),
           ),
-        ));
+        ),
+        body: TabBarView(children: [HomeScreen(), SearchScreen(), NewAndHot()]),
+      ),
+    );
   }
 }
