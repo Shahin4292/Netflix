490b7f9ec522e972a67884c6788b2c52270664e2
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-02-10 11:13:31 +0600

    splash screen all done

---
+++ lib/view/splash_screen/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+import '../../res/components/bottom_nav_bar/bottom_nav_bar.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 4), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => BottomNavBar()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Lottie.asset("assets/json/netflix.json"),
+      ),
+    );
+  }
+}
