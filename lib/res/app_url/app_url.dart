375499066c173e8f5387de4fdc44168a43d7f805
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-02-10 13:32:44 +0600

    add app url almost done

---
+++ lib/res/app_url/app_url.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const apiKey =
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MDhkMTgxZjRlYTk2OTAyYmZiMjE2OGZhMTRhNjMzNSIsIm5iZiI6MTczMjQxNDczMy44MzksInN1YiI6IjY3NDI4ZDBkYWFiNzIyYmZhN2M5MDI5MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.M4cyspnxqkIsxMMQAj2SpRYMTuftpFgDu4VxeYMO-iQ";
+const kBackgroundColor = Colors.black;
+const imageUrl = "https://image.tmdb.org/t/p/w500";
