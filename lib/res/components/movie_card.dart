9f88ca44b09c42db8666adb3c1f15bcc552db050
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-02-10 18:11:21 +0600

    all add done

--- lib/res/components/movie_card.dart
+++ lib/res/components/movie_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:netflix/model/upcoming_movie_model/upcoming_movie_model.dart';
+import 'package:netflix/res/app_url/app_url.dart';
 
 class MovieCard extends StatelessWidget {
   final Future<UpcomingMovieModel> future;
@@ -9,14 +10,30 @@ class MovieCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(future: future, builder: (context, snapshot){
-      var data = snapshot.data!.results;
-      return Column(
-        spacing: 20,
-        children: [
-          Text(text,style: TextStyle(fontWeight: FontWeight.bold)),
-        ],
-      );
-    });
+    return FutureBuilder(
+        future: future,
+        builder: (context, snapshot) {
+          var data = snapshot.data!.results;
+          return Column(
+            spacing: 20,
+            children: [
+              Text(text, style: TextStyle(fontWeight: FontWeight.bold)),
+              Expanded(
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: data.length,
+                    itemBuilder: (context, index) {
+                    final dataVar = data[index].posterPath;
+                      return Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Image.network("$imageUrl$dataVar"),
+                      );
+                    }),
+              )
+            ],
+          );
+        });
   }
 }
