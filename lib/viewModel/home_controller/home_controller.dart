40febd8cbeb770a7253de95555e9a9f98a8ac9e1
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-02-10 15:51:14 +0600

    get api service add done

--- lib/viewModel/home_controller/home_controller.dart
+++ lib/viewModel/home_controller/home_controller.dart
@@ -1,5 +1,7 @@
-import 'package:netflix/model/upcoming_movie_model/upcoming_movie_model.dart';
+import 'dart:convert';
 
+import 'package:netflix/model/upcoming_movie_model/upcoming_movie_model.dart';
+import 'package:http/http.dart' as http;
 import '../../res/app_url/app_url.dart';
 
 var key = '?api_key=$apiKey';
@@ -9,5 +11,12 @@ class HomeController {
   Future<UpcomingMovieModel> getUpcomingMovie() async {
     endPoint = "movie/upcoming";
     final url = "$baseUrl$endPoint$key";
+    final response = await http.get(Uri.parse(url));
+    final data = jsonDecode(response.body);
+    print(data);
+    if (response.statusCode == 200) {
+      return UpcomingMovieModel.fromJson(data);
+    }
+    throw Exception("Failed to load upcoming movie");
   }
 }
