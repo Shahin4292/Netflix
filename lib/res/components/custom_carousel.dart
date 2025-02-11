866fb842545a277a7c818a9127eb1bf70be40c68
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-02-11 10:28:43 +0600

    add cached image

--- lib/res/components/custom_carousel.dart
+++ lib/res/components/custom_carousel.dart
@@ -1,6 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:netflix/model/top_rated_tv_series_movie_model/top_rated_tv_series_movie_model.dart';
+import 'package:netflix/res/app_url/app_url.dart';
 
 class CustomCarousel extends StatelessWidget {
   final TopRatedTvSeriesMovieModel data;
@@ -17,6 +19,11 @@ class CustomCarousel extends StatelessWidget {
             itemCount: data.results.length,
             itemBuilder: (BuildContext context, int index, int realIndex) {
               var url = data.results[index].posterPath.toString();
+              return GestureDetector(
+                child: CachedNetworkImage(
+                  imageUrl: "$imageUrl$url",
+                ),
+              );
             },
             options: CarouselOptions()));
   }
