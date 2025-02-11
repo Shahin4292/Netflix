f06503c4dad50cce6084d854234ec9cf14324fe5
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-02-11 11:26:05 +0600

    add model

---
+++ lib/model/now_playing_movie_model/now_playing_movie_model.dart
@@ -0,0 +1,116 @@
+class NowPlayingMovieModel {
+  Dates dates;
+  int page;
+  List<Result> results;
+  int totalPages;
+  int totalResults;
+
+  NowPlayingMovieModel({
+    required this.dates,
+    required this.page,
+    required this.results,
+    required this.totalPages,
+    required this.totalResults,
+  });
+
+  NowPlayingMovieModel copyWith({
+    Dates? dates,
+    int? page,
+    List<Result>? results,
+    int? totalPages,
+    int? totalResults,
+  }) =>
+      NowPlayingMovieModel(
+        dates: dates ?? this.dates,
+        page: page ?? this.page,
+        results: results ?? this.results,
+        totalPages: totalPages ?? this.totalPages,
+        totalResults: totalResults ?? this.totalResults,
+      );
+}
+
+class Dates {
+  DateTime maximum;
+  DateTime minimum;
+
+  Dates({
+    required this.maximum,
+    required this.minimum,
+  });
+
+  Dates copyWith({
+    DateTime? maximum,
+    DateTime? minimum,
+  }) =>
+      Dates(
+        maximum: maximum ?? this.maximum,
+        minimum: minimum ?? this.minimum,
+      );
+}
+
+class Result {
+  bool adult;
+  String backdropPath;
+  List<int> genreIds;
+  int id;
+  String originalLanguage;
+  String originalTitle;
+  String overview;
+  double popularity;
+  String posterPath;
+  DateTime releaseDate;
+  String title;
+  bool video;
+  double voteAverage;
+  int voteCount;
+
+  Result({
+    required this.adult,
+    required this.backdropPath,
+    required this.genreIds,
+    required this.id,
+    required this.originalLanguage,
+    required this.originalTitle,
+    required this.overview,
+    required this.popularity,
+    required this.posterPath,
+    required this.releaseDate,
+    required this.title,
+    required this.video,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  Result copyWith({
+    bool? adult,
+    String? backdropPath,
+    List<int>? genreIds,
+    int? id,
+    String? originalLanguage,
+    String? originalTitle,
+    String? overview,
+    double? popularity,
+    String? posterPath,
+    DateTime? releaseDate,
+    String? title,
+    bool? video,
+    double? voteAverage,
+    int? voteCount,
+  }) =>
+      Result(
+        adult: adult ?? this.adult,
+        backdropPath: backdropPath ?? this.backdropPath,
+        genreIds: genreIds ?? this.genreIds,
+        id: id ?? this.id,
+        originalLanguage: originalLanguage ?? this.originalLanguage,
+        originalTitle: originalTitle ?? this.originalTitle,
+        overview: overview ?? this.overview,
+        popularity: popularity ?? this.popularity,
+        posterPath: posterPath ?? this.posterPath,
+        releaseDate: releaseDate ?? this.releaseDate,
+        title: title ?? this.title,
+        video: video ?? this.video,
+        voteAverage: voteAverage ?? this.voteAverage,
+        voteCount: voteCount ?? this.voteCount,
+      );
+}
