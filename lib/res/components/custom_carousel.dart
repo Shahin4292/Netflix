129ce6a1ba8365d5f7ab8fbfc6d43732df0a0946
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-02-12 11:12:37 +0600

    almost done

--- lib/res/components/custom_carousel.dart
+++ lib/res/components/custom_carousel.dart
@@ -1,30 +1,57 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:netflix/model/top_rated_tv_series_movie_model/top_rated_tv_series_movie_model.dart';
-import 'package:netflix/res/app_url/app_url.dart';
 
 class CustomCarousel extends StatelessWidget {
-  final TopRatedTvSeriesMovieModel data;
+  const CustomCarousel({
+    super.key,
+    required Future<List> movies,
+  }) : _movies = movies;
 
-  const CustomCarousel({super.key, required this.data});
+  final Future<List> _movies;
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.sizeOf(context);
-    return SizedBox(
-        width: size.width,
-        height: (size.height * 0.33 < 300) ? 300 : size.height * 0.33,
-        child: CarouselSlider.builder(
-            itemCount: data.results.length,
-            itemBuilder: (BuildContext context, int index, int realIndex) {
-              var url = data.results[index].posterPath.toString();
-              return GestureDetector(
-                child: CachedNetworkImage(
-                  imageUrl: "$imageUrl$url",
-                ),
-              );
-            },
-            options: CarouselOptions()));
+    return FutureBuilder<List<dynamic>>(
+      future: _movies,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        }
+
+        if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return Center(child: Text('No movies found.'));
+        }
+
+        final movies = snapshot.data!;
+
+        return Column(
+          children: [
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 400.0,
+                enlargeCenterPage: true,
+                autoPlay: true,
+                aspectRatio: 16 / 9,
+                viewportFraction: 0.8,
+              ),
+              items: movies.map((movie) {
+                final imageUrl = 'https://image.tmdb.org/t/p/w500${movie['poster_path']}';
+                return Container(
+                  margin: EdgeInsets.symmetric(horizontal: 5.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: Image.network(imageUrl, fit: BoxFit.cover),
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
