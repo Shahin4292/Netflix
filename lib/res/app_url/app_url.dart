58ec95171d75ab516bffc51188bd15ffba28b5c1
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-02-14 16:07:00 +0600

    app are all add done

--- lib/res/app_url/app_url.dart
+++ lib/res/app_url/app_url.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 const kBackgroundColor = Colors.black;
 Color backgroundPrimary = const Color(0xFF111015);
 const apiKey = "8f70539a3d9d0d870609558f7a991e62";
+const keys = "&api_key=$apiKey";
 const baseUrl = 'https://api.themoviedb.org/3/';
 const imageUrl = "https://image.tmdb.org/t/p/w500";
 
