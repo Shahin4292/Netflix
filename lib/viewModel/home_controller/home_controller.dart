99d38adabffa712f9d6cb736cb7934c6de9bd58d
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-02-14 13:48:12 +0600

    search function add

--- lib/viewModel/home_controller/home_controller.dart
+++ lib/viewModel/home_controller/home_controller.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'dart:math';
 
+import 'package:netflix/model/search_model/search_model.dart';
 import 'package:netflix/model/upcoming_movie_model/upcoming_movie_model.dart';
 import 'package:http/http.dart' as http;
 import '../../model/top_rated_tv_series_movie_model/top_rated_tv_series_movie_model.dart';
@@ -46,15 +48,23 @@ class HomeController {
     }
   }
 
-  // Future<TopRatedTvSeriesModel> topRatedMovieSeries() async {
-  //   endPoint = "tv/top_rated";
-  //   final url = "$baseUrl$endPoint$key";
-  //   final response = await http.get(Uri.parse(url));
-  //   final data = jsonDecode(response.body);
-  //   print(data);
-  //   if (response.statusCode == 200) {
-  //     return TopRatedTvSeriesModel.fromJson(data);
-  //   }
-  //   throw Exception("Failed to load tv series");
-  // }
+  Future<SearchModel> getSearchMovie(String searchText) async {
+    String bab = "https://api.themoviedb.org/3/search/movie?api_key=8f70539a3d9d0d870609558f7a991e62&query=harry";
+    // String keys =
+    //     "https://api.themoviedb.org/3/search/movie?query=harry&api_key=8f70539a3d9d0d870609558f7a991e62";
+    endPoint = "search/movie?query=$searchText";
+    final url = "$baseUrl$endPoint$key";
+    print("search url : $url");
+    final response = await http.get(Uri.parse(bab), headers: {
+      'Authorization':
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSIsInJvbGUiOiJBRE1JTiIsIl9pZCI6IjY3MjQ1M2I2ZGU0Y2JjNGU1YWUwYjBlMiIsImlhdCI6MTczODQxMTg1MiwiZXhwIjoxNzM5Mjc1ODUyfQ.K5iTqmOIw6sGEuZpBm0UtwJgjabUliiLTOHi0HfhU08"
+    });
+    final data = jsonDecode(response.body);
+    print(data);
+    if (response.statusCode == 200) {
+      print("success");
+      return SearchModel.fromJson(data);
+    }
+    throw Exception("Failed to search movies");
+  }
 }
