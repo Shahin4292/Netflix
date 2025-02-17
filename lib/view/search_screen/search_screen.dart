841ab5593b5c89955c913010bfba4ef60570449f
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-02-17 11:14:39 +0600

    show search image input no text

--- lib/view/search_screen/search_screen.dart
+++ lib/view/search_screen/search_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:netflix/model/movie_recommendation/movie_recommendation.dart';
 import 'package:netflix/res/app_url/app_url.dart';
 import 'package:netflix/res/assets/image_path.dart';
 import 'package:netflix/viewModel/home_controller/home_controller.dart';
@@ -17,6 +18,7 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   TextEditingController searchController = TextEditingController();
   HomeController homeController = HomeController();
+  late Future<MovieRecommendationModel> popularMovie;
   SearchModel? searchModel;
 
   void search(String query) {
@@ -25,6 +27,12 @@ class _SearchScreenState extends State<SearchScreen> {
         searchModel = results;
       });
     });
+  }
+
+  @override
+  void initState() {
+    popularMovie = homeController.getPopularMovie();
+    super.initState();
   }
 
   @override
@@ -84,37 +92,83 @@ class _SearchScreenState extends State<SearchScreen> {
                         ),
                         hintText: "Search"),
                   ),
-                  searchModel == null
-                      ? SizedBox.shrink()
-                      : GridView.builder(
-                          shrinkWrap: true,
-                          itemCount: searchModel!.results.length,
-                          gridDelegate:
-                              SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: 3,
-                                  mainAxisSpacing: 15,
-                                  crossAxisSpacing: 5,
-                                  childAspectRatio: 1.2 / 2),
-                          itemBuilder: (context, index) {
-                            return Column(
-                              children: [
-                                searchController.text.isEmpty
-                                searchModel!.results[index].posterPath ==
-                                        null
-                                    ? Image.asset(ImagePath.netflix,height: 170)
-                                    : Container(
-                                        margin: EdgeInsets.only(right: 15),
-                                        width: MediaQuery.of(context).size.width,
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(8),
-                                          image: DecorationImage(
-                                              image: NetworkImage(
-                                            "$imageUrl${searchModel?.results[index].posterPath}",
-                                          )),
-                                        )),
-                              ],
-                            );
+                  searchController.text.isEmpty
+                      ? FutureBuilder<MovieRecommendationModel>(
+                          future: popularMovie,
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              var data = snapshot.data?.results;
+                              return Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(
+                                      height: 30,
+                                    ),
+                                    Text(
+                                      "Search Movie",
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    ListView.builder(
+                                      shrinkWrap: true,
+                                      physics: NeverScrollableScrollPhysics(),
+                                      scrollDirection: Axis.vertical,
+                                      itemCount: data!.length,
+                                      itemBuilder: (context, index) {
+                                        return Container(
+                                          height: 150,
+                                          decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(20)),
+                                          child: Image.network(
+                                            '$imageUrl${data[index].posterPath}',
+                                            fit: BoxFit.fitHeight,
+                                          ),
+                                        );
+                                      },
+                                    )
+                                  ]);
+                            } else {
+                              return const SizedBox.shrink();
+                            }
                           })
+                      : searchModel == null
+                          ? SizedBox.shrink()
+                          : GridView.builder(
+                              shrinkWrap: true,
+                              itemCount: searchModel!.results.length,
+                              gridDelegate:
+                                  SliverGridDelegateWithFixedCrossAxisCount(
+                                      crossAxisCount: 3,
+                                      mainAxisSpacing: 15,
+                                      crossAxisSpacing: 5,
+                                      childAspectRatio: 1.2 / 2),
+                              itemBuilder: (context, index) {
+                                return Column(
+                                  children: [
+                                    searchModel!.results[index].posterPath ==
+                                            null
+                                        ? Image.asset(ImagePath.netflix,
+                                            height: 170)
+                                        : Container(
+                                            margin: EdgeInsets.only(right: 15),
+                                            width: MediaQuery.of(context)
+                                                .size
+                                                .width,
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(8),
+                                              image: DecorationImage(
+                                                  image: NetworkImage(
+                                                "$imageUrl${searchModel?.results[index].posterPath}",
+                                              )),
+                                            )),
+                                  ],
+                                );
+                              })
                 ],
               ),
             ),
